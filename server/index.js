d2e41fd541b00743644189b53d7d00d424bc3933
siyam haider <i211571@nu.edu.pk>
2022-09-27 23:09:36 -0400

    initial set up of backend setting up port and connecting to db

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// const express = require ('express')
+// const cors = require ('cors')
+// const mysql = require ('mysql')
+
+const express = require('express')
+
+const mysql = require('mysql')
+require('dotenv').config()
+const PORT = process.env.PORT
+
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const db = mysql.createConnection({
+    user: 'sqluser',
+    host: 'localhost',
+    password:'password',
+    database: 'employeesystem'
+})
+
+app.listen(PORT,() =>{
+    console.log("you are connnected to port 4001")
+    db.connect(function(err){
+        if(err)throw err
+        console.log('db connected')
+    })
+})
